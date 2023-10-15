c3450ab6d61c57616b8bf99c93b128550e62b6ec
olek1305 <olek1305@gmail.com>
2023-10-15 23:08:00 +0200

    Design answers table schema and rename answers column to answers_count in the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -24,7 +24,7 @@
                                         <strong>{{$question->votes}}</strong> {{str_plural('vote', $question->votes) }}
                                     </div>
                                     <div class="status {{ $question->status }}">
-                                        <strong>{{$question->answers}}</strong> {{str_plural('answer', $question->answers) }}
+                                        <strong>{{$question->answers_count}}</strong> {{str_plural('answer', $question->answers_count) }}
                                     </div>
                                     <div class="view">
                                         {{$question->views . " " . str_plural('view', $question->views) }}
@@ -34,12 +34,12 @@
                                     <div class="d-flex align-items-center">
                                         <h3 class="mt-0"> <a href="{{ $question->url }}">{{ $question->title }}</a> </h3>
                                         <div class="ml-auto">
-                                            @can ('update', $question))
+                                            @can ('update', $question)
                                                 <a href="{{ route('questions.edit', $question->id)  }}" class="btn btn-sm btn-outline-info text-dark">Edit</a>
                                             @endcan
 
 
-                                            @can('delete', $question))
+                                            @can('delete', $question)
                                             <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
                                                 @method('DELETE')
                                                 @csrf
