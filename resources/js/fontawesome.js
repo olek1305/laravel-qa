b9b67faca2fe34e8784a0faf2204f9102466b35d
olek1305 <olek1305@gmail.com>
2023-10-23 16:40:08 +0200

    added fontawesome and downgrade bootstrap to fix scss,

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/fontawesome';
+import faCaretUp from '@fortawesome/fontawesome-free-solid/faCaretUp';
+import faCaretDown from '@fortawesome/fontawesome-free-solid/faCaretDown';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+
+fontawesome.library.add(faCaretUp, faCaretDown, faCheck, faStar);
