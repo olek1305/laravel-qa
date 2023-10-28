9835dd874e8935c52697647a3ef6a70200f01cff
olek1305 <olek1305@gmail.com>
2023-10-28 17:35:36 +0200

    Update best_answer_id on questions table when an answer removed using Eloquent event, fix body answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -6,7 +6,8 @@
                     <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
                 </div>
                 <hr>
-                @include('layouts._messages')
+                @include ('layouts._messages')
+
                 @foreach ($answers as $answer)
                     <div class="media">
                         <div class="d-fex flex-column vote-controls">
@@ -17,18 +18,19 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                            <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
                                 <i class="fas fa-check fa-2x"></i>
                             </a>
                         </div>
                         <div class="media-body">
+                            {!! $answer->body_html !!}
                             <div class="row">
                                 <div class="col-4">
                                     <div class="ml-auto">
                                         @can ('update', $answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id])  }}" class="btn btn-sm btn-outline-info text-dark">Edit</a>
+                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
-                                        @can('delete', $answer)
+                                        @can ('delete', $answer)
                                             <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
                                                 @method('DELETE')
                                                 @csrf
@@ -37,12 +39,8 @@
                                         @endcan
                                     </div>
                                 </div>
+                                <div class="col-4"></div>
                                 <div class="col-4">
-
-
-                                </div>
-                                {!! $answer->body_html !!}
-                                <div class="float-right">
                                     <span class="text-muted">Answered {{ $answer->created_date }}</span>
                                     <div class="media mt-2">
                                         <a href="{{ $answer->user->url }}" class="pr-2">
