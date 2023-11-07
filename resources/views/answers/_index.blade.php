a649c2b47fb2eb43eafed4b207676440bfcba1de
olek1305 <olek1305@gmail.com>
2023-11-07 21:36:25 +0100

    Make the vote answer working

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -11,13 +11,27 @@
                 @foreach ($answers as $answer)
                     <div class="media">
                         <div class="d-fex flex-column vote-controls">
-                            <a title="This answer is useful" class="vote-up">
+                            <a title="This answer is useful"
+                               class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+                               onclick="event.preventDefault(); document.getElementById(('up-vote-answer-{{ $answer->id }}')).submit();"
+                            >
                                 <i class="fas fa-caret-up fa-3x"></i>
                             </a>
-                            <span class="votes-count">1230</span>
-                            <a title="This answer is not useful" class="vote-down off">
+                            <form id="up-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="post" style="display: none;">
+                                @csrf
+                                <input type="hidden" name="vote" value="1">
+                            </form>
+                            <span class="votes-count">{{ $answer->votes_count }}</span>
+                            <a title="This answer is not useful"
+                               class="vote-down {{ Auth::guest() ? 'off' : '' }}"
+                               onclick="event.preventDefault(); document.getElementById(('down-vote-answer-{{ $answer->id }}')).submit();"
+                            >
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
+                            <form id="down-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="post" style="display: none;">
+                                @csrf
+                                <input type="hidden" name="vote" value="-1">
+                            </form>
                             @can('accept', $answer)
                                 <a title="Mark this answer as best answer"
                                    class="{{ $answer->status }} mt-2"
@@ -60,7 +74,7 @@
                                     <span class="text-muted">Answered {{ $answer->created_date }}</span>
                                     <div class="media mt-2">
                                         <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}">
+                                            <img src="{{ $answer->user->avatar }}" alt="avatar">
                                         </a>
                                         <div class="media-body mt-1">
                                             <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
