fcb154e4d65528b9dc6f1ec9e8ce3d2443868c32
olek1305 <olek1305@gmail.com>
2023-11-12 20:57:20 +0100

    My bad with phpstorm, didn't know how works with git with phpstorm, implement DRY principle on answer and question models

--- app/Answer.php
+++ app/Answer.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    use VotableTrait;
+
     protected $fillable = ['body', 'user_id'];
-    
+
     public function question()
     {
         return $this->belongsTo(Question::class);
@@ -22,17 +24,17 @@ class Answer extends Model
     {
         return \Parsedown::instance()->text($this->body);
     }
-    
+
     public static function boot()
     {
         parent::boot();
 
         static::created(function ($answer) {
-            $answer->question->increment('answers_count');                     
-        });        
+            $answer->question->increment('answers_count');
+        });
 
-        static::deleted(function ($answer) {            
-            $answer->question->decrement('answers_count');            
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
         });
     }
 
@@ -54,20 +56,5 @@ class Answer extends Model
     public function isBest()
     {
         return $this->id === $this->question->best_answer_id;
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
