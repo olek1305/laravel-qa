327a182ca7255d2d9129eac62660d17c69f2b286
olek1305 <olek1305@gmail.com>
2023-12-13 14:21:54 +0100

    Create some endpoints for answer resource

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,12 @@ use Illuminate\Http\Request;
 */
 Route::post('/token', 'Auth\LoginController@getToken');
 Route::get('/questions', 'Api\QuestionsController@index');
+Route::get('/questions/{question}/answers', 'Api\AnswersController@index');
 Route::get('/questions/{question}-{slug}', 'Api\QuestionDetailsController');
 
 Route::middleware(['auth:api'])->group(function() {
     Route::apiResource('/questions', 'Api\QuestionsController')->except('index');
+    Route::apiResource('/questions.answers', 'Api\AnswersController')->except('index');
 });
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
