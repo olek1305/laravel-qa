a20e006d7fa36546837dfcc7a39fa25e56bd09a5
olek1305 <olek1305@gmail.com>
2023-12-15 12:23:06 +0100

    Create Api endpoints for Authentication

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/token', 'Auth\LoginController@getToken');
+
+//Auth
+Route::post('/login', 'Api\Auth\LoginController@store');
+Route::delete('/logout', 'Api\Auth\LoginController@destroy')->middleware('auth:api');
+Route::post('/register', 'Api\Auth\RegisterController');
 
 Route::get('/questions', 'Api\QuestionsController@index');
 Route::get('/questions/{question}/answers', 'Api\AnswersController@index');
