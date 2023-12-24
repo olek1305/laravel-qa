1699ceb02bcdac4ba5bd8c49d2b33bfcdac34abb
olek1305 <olek1305@gmail.com>
2023-12-24 15:02:08 +0100

    Laravel upgrade to 7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -40,7 +42,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+//            'bindings',
         ],
     ];
 
