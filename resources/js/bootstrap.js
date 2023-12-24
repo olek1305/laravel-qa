ef87e2c1acc5a8b2aa0dccfe2fd079f4558f96f5
olek1305 <olek1305@gmail.com>
2023-12-24 17:30:04 +0100

    fix js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,7 +23,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = window.Urls.api || 'http://localhost:8000/api'; //example: axios.get('question')
+window.axios.defaults.baseURL = (window.Urls && window.Urls.api) || 'http://localhost:8000/api'; //example: axios.get('question')
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
